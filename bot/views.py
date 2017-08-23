4cbe84922ca70c23ea79fd446e7deec1833d81a2
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-08-23 19:16:24 +0300

    First commit

---
+++ bot/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import telepot
+import urllib3, json
+
+# Create your views here.
+
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (
+    urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+secret = "f893df61-ee35-4a84-87fd-f0698b7438f7"
+bot = telepot.Bot('408802721:AAHmVxkphGXoPWhQruHOLMDubWCkLk-vfCE')
+bot.setWebhook("https://timeismoney.pythonanywhere.com/{}".format(secret), max_connections=1)
+
+
+def telegram_webhook(request):
+    update = json.loads(request.body)
+    if "message" in update:
+        text = update["message"]["text"]
+        chat_id = update["message"]["chat"]["id"]
+        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+    return "OK"
