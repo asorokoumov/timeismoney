3c9f666ce2672c85e373ea3801839cef8bd623ca
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-08-23 19:39:16 +0300

    Trying to launch django bot

--- bot/urls.py
+++ bot/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/f893df61-ee35-4a84-87fd-f0698b7438f7/408802721:AAHmVxkphGXoPWhQruHOLMDubWCkLk-vfCE/$',
+    url(r'^f893df61-ee35-4a84-87fd-f0698b7438f7/408802721:AAHmVxkphGXoPWhQruHOLMDubWCkLk-vfCE/$',
         views.telegram_webhook, name='telegram_webhook'),
 
 ]
--- bot/views.py
+++ bot/views.py
@@ -3,20 +3,18 @@ import telepot
 import urllib3, json
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (
-    urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+print("ITS ALIVE")
 
 secret = "f893df61-ee35-4a84-87fd-f0698b7438f7"
 bot = telepot.Bot('408802721:AAHmVxkphGXoPWhQruHOLMDubWCkLk-vfCE')
 bot.setWebhook("https://timeismoney.pythonanywhere.com/{}".format(secret), max_connections=1)
 
 
+@csrf_exempt
 def telegram_webhook(request):
+    print("I'm done")
     update = json.loads(request.body)
     if "message" in update:
         text = update["message"]["text"]
