8e6588c8bd253c57d1c16cd980a534c0bec36bed
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-08-24 09:09:58 +0300

    Add proxy for pythonanywhere

--- bot/views.py
+++ bot/views.py
@@ -7,6 +7,13 @@ from django.views.decorators.csrf import csrf_exempt
 
 print("ITS ALIVE")
 
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+
 secret = "f893df61-ee35-4a84-87fd-f0698b7438f7"
 bot = telepot.Bot('408802721:AAHmVxkphGXoPWhQruHOLMDubWCkLk-vfCE')
 bot.setWebhook("https://timeismoney.pythonanywhere.com/{}".format(secret), max_connections=1)
