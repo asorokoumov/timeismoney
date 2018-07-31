31c9a927fb1e27069a0362f596b483bd57ccf91b
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2018-07-31 09:25:35 +0300

    Register models

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Worker, Project, TimeSheet, LoggingStep, Reminder
+
+admin.site.register(Worker)
+admin.site.register(Project)
+admin.site.register(TimeSheet)
+admin.site.register(LoggingStep)
+admin.site.register(Reminder)
